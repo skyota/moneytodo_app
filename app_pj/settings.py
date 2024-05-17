f1e1ead8964e2b9c2d9e15575114977e58067ec1
skyota <117239040+skyota@users.noreply.github.com>
2024-05-17 16:11:09 +0900

    Update settings.py

--- app_pj/settings.py
+++ app_pj/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         "ENGINE": os.environ.get("DB_ENGINE"),
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
-        "PASSWORD": 'os.environ.get("DB_PASSWORD")',
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
     }
